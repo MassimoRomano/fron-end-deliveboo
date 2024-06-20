7130d6e8e845a71ab56f452d146615f5cddfb6eb
MarioBroi <inoix16@gmail.com>
2024-06-20 09:11:46 +0200

    edit single restaurant page t print the correct dishes

--- src/components/views/AppRestaurant.vue
+++ src/components/views/AppRestaurant.vue
@@ -42,6 +42,10 @@ export default {
 
 <template>
     <template v-if="restaurants" v-for="restaurant in restaurants">
+        <div>
+            {{ console.log(restaurant) }}
+        </div>
+
         <div class="restaurant-page">
             <div class="container">
                 <section class="info-rest">
@@ -56,7 +60,40 @@ export default {
                     </div>
                 </section>
 
-                <h2>Prodotti</h2>
+                <section>
+                    <div class="row">
+                        <div class="product-info">
+                            <div @click="openModal('Osso di Seppia', '/public/img/deliv.png')"
+                                class="col-2 restaurant-dishes" v-for="dish in restaurant.dishes">
+                                <div class="card-rest">
+                                    <div class="card-body-rest">
+                                        <div class="top-rest">
+                                            <template v-if="dish.image && dish.image.includes('uploads')">
+                                                <div class="card-image">
+                                                    <img :src="base_api_url + 'storage/' + dish.image" alt="">
+                                                </div>
+                                            </template>
+                                            <template v-else>
+                                                <div class="card-image">
+                                                    <img :src="dish.image" :alt="'Image of the dish: ' + dish.name">
+                                                </div>
+                                            </template>
+                                            <!-- ./images -->
+                                        </div>
+                                        <div class="bottom-rest">
+                                            <h3>{{ dish.name }}</h3>
+                                            <p>Indredienti: {{ dish.ingredients }} </p>
+                                            <p>Prezzo: {{ dish.price }}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                </section>
+
+                <!--  <h2>Prodotti</h2>
                 <section class="product">
                     <div class="product-info">
                         <div class="col-2" @click="openModal('Osso di Seppia', '/public/img/deliv.png')">
@@ -82,7 +119,7 @@ export default {
                         </div>
                         <button class="pay"><a href="">Vai al pagamento</a></button>
                     </div>
-                </section>
+                </section> -->
 
                 <!-- Modale -->
                 <div v-if="showModal" class="modal" @click.self="closeModal()">
