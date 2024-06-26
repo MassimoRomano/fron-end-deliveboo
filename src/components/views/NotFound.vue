1a5af1a5fd8f252bb3d8305e2bd8b33be15e9b5c
chrialge <chrialge@gmail.com>
2024-06-26 16:38:12 +0200

    add style

--- src/components/views/NotFound.vue
+++ src/components/views/NotFound.vue
@@ -8,6 +8,9 @@ export default {
             not_found: true,
         };
     },
+    methods: {
+        hasHistory() { return window.history.length > 2 }
+    }
 };
 </script>
 
@@ -15,7 +18,13 @@ export default {
     <div class="container_404">
         <div class="banner_404">
             <h1>404</h1>
-            <h2>Page NotFound!</h2>
+            <h2>Pagina non trovata!</h2>
+            <div class="return_home">
+                <button @click="hasHistory() ? $router.go(-2) : $router.push('/')" class="btn_return">
+                    <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                    Indietro
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -35,7 +44,27 @@ export default {
         text-align: center;
         max-width: 800px;
         width: 90%;
-        border: 2px solid;
+        background-color: rgb(224, 224, 224);
+        border-radius: 10px;
+        box-shadow: rgba(100, 100, 111, 0.4) 3px 3px 29px 0px;
+        color: rgb(142, 121, 248);
+
+        &>h1 {
+            font-size: 90px;
+        }
+
+        .btn_return {
+            background-color: rgb(142, 121, 248);
+            color: white;
+            padding: 0.5rem 0.75rem;
+            border-radius: 5px;
+            border: none;
+
+            &:hover {
+                background-color: rgba(109, 72, 178, 1);
+                cursor: pointer;
+            }
+        }
     }
 }
 </style>
