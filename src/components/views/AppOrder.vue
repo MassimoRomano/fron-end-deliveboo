005426d4538af0090992c194d3e68cc7dfc073d4
MarioBroi <inoix16@gmail.com>
2024-07-01 16:46:52 +0200

    edit order page layout and style

--- src/components/views/AppOrder.vue
+++ src/components/views/AppOrder.vue
@@ -267,7 +267,7 @@ export default {
 <template>
 
     <div class="order-page">
-        <section class="order p-5" v-if="this.total_price > 0">
+        <section class="order_container" v-if="this.total_price > 0">
 
             <router-link :to="{ path: '/restaurant/' + this.restaurant_slug }" class="btn_back">
                 <span>
@@ -276,7 +276,7 @@ export default {
             </router-link>
             <!-- /back to restaurant -->
 
-            <div class="container">
+            <div>
                 <!-- alert di successo del campo form lato laravel-->
                 <template v-if="success">
                     <div class="alert alert-success">
@@ -304,163 +304,16 @@ export default {
                 </template>
                 <!-- /errors -->
 
-                <h2 v-if="!this.orderDone" class="fw_bold text_center">
+                <h1 v-if="!this.orderDone" class="order_title">
                     Inserisci i tuoi dati per completare l'ordine
-                </h2>
+                </h1>
                 <!-- Title -->
 
-                <div class="row">
-                    <div class="col">
-                        <form v-if="!this.orderDone" action="" method="post" @submit.prevent="newOrder">
+                <div class="order_row">
 
-                            <div class="mb-3">
-                                <label for="customer_name" class="form-label">Nome *</label>
-                                <input type="text" class="form-control pad-3" name="customer_name" id="customer_name"
-                                    aria-describedby="helpId" v-model="customer_name"
-                                    :class="{ 'border-red': formErrors.customer_name }" />
-                                <!-- errore di validazione lato laravel -->
-                                <template v-if="formErrors.customer_name">
-                                    <div class="text-red" v-for="error in formErrors.customer_name" :key="error">
-                                        {{ error }}
-                                    </div>
-                                </template>
-                            </div>
-                            <!-- /name -->
-
-                            <div class="mb-3">
-                                <label for="customer_lastname" class="form-label">Cognome *</label>
-                                <input type="text" class="form-control pad-3" name="customer_lastname"
-                                    id="customer_lastname" aria-describedby="helpId" v-model="customer_lastname"
-                                    :class="{ 'border-red': formErrors.customer_lastname }" />
-                                <!-- errore di validazione lato laravel -->
-                                <template v-if="formErrors.customer_lastname">
-                                    <div class="text-red" v-for="error in formErrors.customer_lastname" :key="error">
-                                        {{ error }}
-                                    </div>
-                                </template>
-                            </div>
-                            <!-- /lastname -->
-
-                            <div class="mb-3">
-                                <label for="customer_address" class="form-label">Indirizzo *</label>
-                                <input type="text" class="form-control pad-3" name="customer_address"
-                                    id="customer_address" aria-describedby="helpId" v-model="customer_address"
-                                    :class="{ 'border-red': formErrors.customer_address }" />
-                                <!-- errore di validazione lato laravel -->
-                                <template v-if="formErrors.customer_address">
-                                    <div class="text-red" v-for="error in formErrors.customer_address" :key="error">
-                                        {{ error }}
-                                    </div>
-                                </template>
-                            </div>
-                            <!-- /address -->
-
-                            <div class="mb-3">
-                                <label for="customer_phone_number" class="form-label">Telefono *</label>
-                                <input type="text" class="form-control pad-3" name="customer_phone_number"
-                                    id="customer_phone_number" aria-describedby="helpId" v-model="customer_phone_number"
-                                    :class="{ 'border-red': formErrors.customer_phone_number }" />
-                                <!-- errore di validazione lato laravel -->
-                                <template v-if="formErrors.customer_phone_number">
-                                    <div class="text-red" v-for="error in formErrors.customer_phone_number"
-                                        :key="error">
-                                        {{ error }}
-                                    </div>
-                                </template>
-                            </div>
-                            <!-- /phone number -->
-
-                            <div class="mb-3">
-                                <div class="mb-3">
-                                    <label for="customer_email" class="form-label">Email *</label>
-                                    <input type="email" class="form-control pad-3" name="customer_email"
-                                        id="customer_email" v-model="customer_email"
-                                        :class="{ 'border-red': formErrors.customer_email }" />
-                                    <!-- errore di validazione lato laravel -->
-                                    <template v-if="formErrors.customer_email">
-                                        <div class="text-red" v-for="error in formErrors.customer_email" :key="error">
-                                            {{ error }}
-                                        </div>
-                                    </template>
-                                </div>
-                            </div>
-                            <!-- /email -->
-
-                            <div class="mb-3">
-                                <label for="customer_note" class="form-label">Note</label>
-                                <textarea class="form-control pad-3" name="customer_note" id="customer_note" rows="3"
-                                    v-model="customer_note"
-                                    :class="{ 'border-red': formErrors.customer_note }"></textarea>
-                                <!-- errore di validazione lato laravel -->
-                                <template v-if="formErrors.customer_note">
-                                    <div class="text-red" v-for="error in formErrors.customer_note" :key="error">
-                                        {{ error }}
-                                    </div>
-                                </template>
-                            </div>
-                            <!-- /notes -->
-
-                            <button @click="this.cantYouPay = false" id="submit-button" type="submit" class="btn_pay"
-                                :disabled="loading">
-                                <template v-if="!loading">
-                                    <span>Paga ora</span>
-                                </template>
-                                <template v-else>
-                                    <i class="fa-regular fa-paper-plane px-1"></i>
-                                    Attendi....
-                                </template>
-                            </button>
-                        </form>
-                        <!-- /form -->
-
-                        <template v-if="orderMessage">
-                            {{ orderCompleted() }}
-                            {{ (this.formErrors = null), (this.paymentMessage = '') }}
-                            <div class="alert alert-success mt-3">
-                                <div class="info">{{ orderMessage }}</div>
-                            </div>
-                            <router-link :to="{ name: 'Home' }" class="btn_pay">
-                                <span>
-                                    Torna alla home
-                                </span>
-                            </router-link>
-                        </template>
-                        <!-- /back to home -->
-
-                    </div>
-                    <!-- /.col -->
-
-                    <div class="col">
-                        <div class="my_container">
-                            <!-- Messaggio per la carta rifiutata -->
-                            <div id="dropin-wrapper">
-                                <div id="checkout-message"></div>
-                                <div id="dropin-container"></div>
-                                <AppLoading v-if="isLoading" />
-                            </div>
-
-                            <!-- Messaggio per la carta accettata -->
-                            <template v-if="paymentMessage">
-                                <div class="alert alert-success">
-                                    <div class="info">{{ paymentMessage }}</div>
-                                </div>
-                            </template>
-
-                            <template v-if="this.cantYouPay">
-                                <div class="alert alert-danger">
-                                    <div class="info">{{ this.errorMessage }}</div>
-                                </div>
-                            </template>
-                            
-
-                        </div>
-                        <!-- /pagamento -->
-                    </div>
-                    <!-- /.col -->
-
-                    <div class="col">
-                        <div class="card">
-                            <h3>Riepilogo ordine</h3>
+                    <div class="recap_col">
+                        <div class="recap_card">
+                            <h3 class="recap_title">Riepilogo ordine</h3>
                             <p class="restaurant_name">Ristorante: {{ restaurant_name }}</p>
                             <div>
                                 <ul>
@@ -469,7 +322,7 @@ export default {
                                     </li>
                                 </ul>
                             </div>
-                            <p v-if="total_price"> <span class="fw_bold">Totale Ordine:</span>
+                            <p v-if="total_price"> <span class="tot_order">Totale Ordine:</span>
                                 {{ total_price.toFixed(2) }} &euro;</p>
 
                             <template v-if="orderMessage">
@@ -497,6 +350,145 @@ export default {
                         </div>
                         <!-- /.card -->
                     </div>
+                    <!-- /.recap_col -->
+
+                    <div class="order_col">
+
+                        <form v-if="!this.orderDone" action="" method="post" @submit.prevent="newOrder">
+
+                            <div class="order_form">
+                                <label for="customer_name" class="form_label">
+                                    Nome <span class="mandatory">*</span>
+                                </label>
+                                <input type="text" class="form-control pad-3" name="customer_name" id="customer_name"
+                                    aria-describedby="helpId" v-model="customer_name"
+                                    :class="{ 'border-red': formErrors.customer_name }" />
+                                <!-- errore di validazione lato laravel -->
+                                <template v-if="formErrors.customer_name">
+                                    <div class="text-red" v-for="error in formErrors.customer_name" :key="error">
+                                        {{ error }}
+                                    </div>
+                                </template>
+                            </div>
+                            <!-- /name -->
+
+                            <div class="order_form">
+                                <label for="customer_lastname" class="form-label">
+                                    Cognome <span class="mandatory">*</span>
+                                </label>
+                                <input type="text" class="form-control pad-3" name="customer_lastname"
+                                    id="customer_lastname" aria-describedby="helpId" v-model="customer_lastname"
+                                    :class="{ 'border-red': formErrors.customer_lastname }" />
+                                <!-- errore di validazione lato laravel -->
+                                <template v-if="formErrors.customer_lastname">
+                                    <div class="text-red" v-for="error in formErrors.customer_lastname" :key="error">
+                                        {{ error }}
+                                    </div>
+                                </template>
+                            </div>
+                            <!-- /lastname -->
+
+                            <div class="order_form">
+                                <label for="customer_address" class="form-label">
+                                    Indirizzo <span class="mandatory">*</span>
+                                </label>
+                                <input type="text" class="form-control pad-3" name="customer_address"
+                                    id="customer_address" aria-describedby="helpId" v-model="customer_address"
+                                    :class="{ 'border-red': formErrors.customer_address }" />
+                                <!-- errore di validazione lato laravel -->
+                                <template v-if="formErrors.customer_address">
+                                    <div class="text-red" v-for="error in formErrors.customer_address" :key="error">
+                                        {{ error }}
+                                    </div>
+                                </template>
+                            </div>
+                            <!-- /address -->
+
+                            <div class="order_form">
+                                <label for="customer_phone_number" class="form-label">
+                                    Telefono <span class="mandatory">*</span>
+                                </label>
+                                <input type="text" class="form-control pad-3" name="customer_phone_number"
+                                    id="customer_phone_number" aria-describedby="helpId" v-model="customer_phone_number"
+                                    :class="{ 'border-red': formErrors.customer_phone_number }" />
+                                <!-- errore di validazione lato laravel -->
+                                <template v-if="formErrors.customer_phone_number">
+                                    <div class="text-red" v-for="error in formErrors.customer_phone_number"
+                                        :key="error">
+                                        {{ error }}
+                                    </div>
+                                </template>
+                            </div>
+                            <!-- /phone number -->
+
+                            <div class="order_form">
+                                <label for="customer_email" class="form-label">
+                                    Email <span class="mandatory">*</span>
+                                </label>
+                                <input type="email" class="form-control pad-3" name="customer_email" id="customer_email"
+                                    v-model="customer_email" :class="{ 'border-red': formErrors.customer_email }" />
+                                <!-- errore di validazione lato laravel -->
+                                <template v-if="formErrors.customer_email">
+                                    <div class="text-red" v-for="error in formErrors.customer_email" :key="error">
+                                        {{ error }}
+                                    </div>
+                                </template>
+                            </div>
+                            <!-- /email -->
+
+                            <div class="order_form">
+                                <label for="customer_note" class="form-label">Note</label>
+                                <textarea class="form-control pad-3" name="customer_note" id="customer_note" rows="3"
+                                    v-model="customer_note"
+                                    :class="{ 'border-red': formErrors.customer_note }"></textarea>
+                                <!-- errore di validazione lato laravel -->
+                                <template v-if="formErrors.customer_note">
+                                    <div class="text-red" v-for="error in formErrors.customer_note" :key="error">
+                                        {{ error }}
+                                    </div>
+                                </template>
+                            </div>
+                            <!-- /notes -->
+
+                            <div class="payment_container">
+                                <!-- Messaggio per la carta rifiutata -->
+                                <div id="dropin-wrapper">
+                                    <div id="checkout-message"></div>
+                                    <div id="dropin-container"></div>
+                                </div>
+                            </div>
+                            <!-- /pagamento -->
+
+                            <button @click="this.cantYouPay = false" id="submit-button" type="submit" class="btn_pay"
+                                :disabled="loading">
+                                <template v-if="!loading">
+                                    <span>Paga ora</span>
+                                </template>
+                                <template v-else>
+                                    <i class="fa-regular fa-paper-plane px-1"></i>
+                                    Attendi....
+                                </template>
+                            </button>
+                        </form>
+                        <!-- /form -->
+
+                        <AppLoading v-if="isLoading" class="order_loading" />
+
+                        <template v-if="orderMessage">
+                            {{ orderCompleted() }}
+                            {{ (this.formErrors = null), (this.paymentMessage = '') }}
+                            <div class="alert alert-success mt-3">
+                                <div class="info">{{ orderMessage }}</div>
+                            </div>
+                            <router-link :to="{ name: 'Home' }" class="btn_pay">
+                                <span>
+                                    Torna alla home
+                                </span>
+                            </router-link>
+                        </template>
+                        <!-- /back to home -->
+
+                    </div>
                     <!-- /.col -->
 
                 </div>
@@ -520,7 +512,7 @@ export default {
 
 <style>
 @import '../css/_order.css';
-
+/* 
 .alert {
     padding: 10px;
     display: flex;
@@ -565,5 +557,5 @@ export default {
 
 .text_center {
     text-align: center;
-}
+} */
 </style>
